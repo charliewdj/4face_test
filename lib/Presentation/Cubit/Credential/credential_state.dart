0bf92ae8f4d080c48e64989e1983ee6ba1834c72
Charlie <charlie.reamur@gmail.com>
2023-07-31 18:50:28 +0900

    charlie_20230731_createProject2

---
+++ lib/Presentation/Cubit/Credential/credential_state.dart
@@ -0,0 +1,10 @@
+part of 'credential_cubit.dart';
+
+abstract class CredentialState extends Equatable {
+  const CredentialState();
+}
+
+class CredentialInitial extends CredentialState {
+  @override
+  List<Object> get props => [];
+}
